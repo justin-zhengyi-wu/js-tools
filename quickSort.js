00066aabce2bf960bd8221f0f7045ff2fc6f5d29
justin-zhengyi-wu <justin.zhengyi.wu@gmail.com>
2013-12-17 14:36:29 +0800

    Update quickSort.js

--- quickSort.js
+++ quickSort.js
@@ -18,3 +18,43 @@ var quickSort = function(arr) {
     }
     return quickSort(left).concat([pivot]).concat(right);
 }
+
+var quickSort = function(arr) {
+    if (!arr) {
+        return null;
+    }
+    if (arr.length <= 1) {
+        return arr;
+    }
+    var left = [], right = [];
+    var pivotIndex = Math.floor(arr.length / 2);
+    var pivot = arr.splice(pivotIndex, 1)[0];
+    for (var i = 0; i < arr.length, i++) {
+        if (arr[i] < pivot || (arr[i] == pivot && i < pivotIndex)) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat([pivot]).concat(right);
+}
+
+var quickSort = function(arr, key) {
+    if (!arr) {
+        return null;
+    }
+    if (arr.length <= 1) {
+        return arr;
+    }
+    var left = [], right = [];
+    var pivotIndex = Math.floor(arr.length / 2);
+    var pivot = arr.splice(pivotIndex, 1)[0];
+    for (var i = 0; i < arr.length, i++) {
+        if (!key && ((arr[i] < pivot) || (arr[i] == pivot && i < pivotIndex)) || (key && (arr[i][key] < pivot[key] || (arr[i][key] == pivot[key] && i < pivotIndex)))) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat([pivot]).concat(right);
+}
