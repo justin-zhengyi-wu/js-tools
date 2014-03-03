d0fd15ed3b557da18aae316dee0abe749a2a2160
Justin Wu <justin@darkstar.example.net>
2014-03-03 11:15:06 +0800

    Reorganize structure of folders.

--- argument2Array.js
+++
@@ -1,4 +0,0 @@
-/**
- * Convert the function arguments to be a real array.
- */
-var argArray = Array.prototype.slice.call(arguments);
--- generateRandomNumberInRange.js
+++
@@ -1,9 +0,0 @@
-/**
- * Generate a random number within a range.
- * @param min The minimum value of the range.
- * @param max The maximum value of the range.
- * @return A random number within the range.
- */
-function generateRandomNumberInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
