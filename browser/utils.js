9fd7916625e3a62cb1e575aa82fe84272d08f9f3
justin-zhengyi-wu <justin.zhengyi.wu@gmail.com>
2015-02-04 22:20:55 +0800

    Update utils.js
    Detect OS platform

--- browser/utils.js
+++ browser/utils.js
@@ -1,1 +1,4 @@
-var isMac = /macintosh|mac os x/.test(navigator.userAgent.toLowerCase());
+var isWin = /win(?:32|64|dows)/i.test(navigator.platform);
+var isMac = /mac/i.test(navigator.platform);
+// var isMac = /macintosh|mac os x/i.test(navigator.userAgent);
+var isUnix = /(?:X11)|(?:Linux)/.test(navigator.platform) && !isWin && !isMac;
