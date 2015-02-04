f4c94e64ae5f8731491e330a50277b2eb41359ba
justin-zhengyi-wu <justin.zhengyi.wu@gmail.com>
2015-02-04 22:22:11 +0800

    Update utils.js

--- browser/utils.js
+++ browser/utils.js
@@ -1,4 +1,4 @@
 var isWin = /win(?:32|64|dows)/i.test(navigator.platform);
 var isMac = /mac/i.test(navigator.platform);
 // var isMac = /macintosh|mac os x/i.test(navigator.userAgent);
-var isUnix = /(?:X11)|(?:Linux)/.test(navigator.platform) && !isWin && !isMac;
+var isUnix = /X11|Linux/.test(navigator.platform) && !isWin && !isMac;
